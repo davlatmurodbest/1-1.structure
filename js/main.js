72505f11901ba3d54a07b091007500452936a7bc
davlatmurodbest <davladmurodalmurodov@gmail.com>
2023-11-13 22:32:37 +0500

    sleep

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var username = Number(prompt('son yozing'))
+var username2 = Number(prompt('son yozing'))
+
+console.log(username);
+console.log(username2);
+
+var plus = console.log(++username + username2);
+
+alert(++plus)
+
+console.log(typeof plus);
